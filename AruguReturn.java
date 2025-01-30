0898e113a756e2aa744b8b810f898e44b60b2e05
punnammanikanta <PUNNAMMANIKANTA52@GMAIL.COM>
2025-01-30 13:09:10 +0530

    Add files via upload

---
+++ AruguReturn.java
@@ -0,0 +1,14 @@
+public class AruguReturn {
+   
+        public static int sum(int num1,int num2){
+            return num1+num2;
+        }
+        public static void main(String[]args){
+            int number1=6;
+            int number2=13;
+            int result=sum(number1,number2);
+            System.out.println("the sum of"+number1+"and "+number2+"is:"+result);
+        }
+    }
+    
+
